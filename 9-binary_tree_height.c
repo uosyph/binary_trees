aeb6f89c2bb0f8bbb1b121a88d8274954f405e21
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-01 08:15:38 +0300

    Statement change

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_left;
 	size_t height_right;
 
-	if (tree == NULL || !(tree->left) && !(tree->right))
+	if (tree == NULL)
+		return (0);
+
+	if (!(tree->left) && !(tree->right))
 		return (0);
 
 	height_left = binary_tree_height(tree->left);
