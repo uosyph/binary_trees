a9c40fcf99b850fa432da20704c098b37a62f8f6
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-02 03:50:44 +0300

    change STMT place

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,13 +9,15 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL ||
-		binary_tree_is_full(tree->right) == 0 ||
-		binary_tree_is_full(tree->left) == 0)
+	if (tree == NULL)
 		return (0);
 
 	if (tree->right == NULL && tree->left == NULL)
 		return (1);
+
+	if (binary_tree_is_full(tree->right) == 0 ||
+		binary_tree_is_full(tree->left) == 0)
+		return (0);
 
 	return (1);
 }
