5f6691d7c046f1bdb74e26347f23256e280951d6
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-03 02:56:21 +0300

    Revert all recent changes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height_left;
+	size_t height_right;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t height_right = binary_tree_height(tree->right);
-	height_right++;
-	size_t height_left = binary_tree_height(tree->left);
+	height_left = binary_tree_height(tree->left);
 	height_left++;
+	height_right = binary_tree_height(tree->right);
+	height_right++;
 
 	if (height_left > height_right)
 		return (height_left);
