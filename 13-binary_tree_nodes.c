3b2fc26cb235dc69c8204fc5585be1e94cc54f4f
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-02 03:25:08 +0300

    Return 0 if tree is NULL

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,8 +9,12 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
 	if (tree->right || tree->left)
 		return ((binary_tree_nodes(tree->right) + 1) +
 				(binary_tree_nodes(tree->left) + 1) - 1);
+
 	return (0);
 }
