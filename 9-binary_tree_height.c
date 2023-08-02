1308bcb55c16bb539a7233b263517bb9ec536144
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-03 02:48:51 +0300

    minor change

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,17 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree == NULL || (!(tree->left) && !(tree->right)))
+		return (0);
+
 	size_t height_right = binary_tree_height(tree->right);
-	size_t height_left = binary_tree_height(tree->left);
-
-	if (tree == NULL)
-		return (0);
-
-	if (!(tree->left) && !(tree->right))
-		return (0);
-
-	height_left++;
 	height_right++;
+	size_t height_left = binary_tree_height(tree->left);
+	height_left++;
 
 	if (height_left > height_right)
 		return (height_left);
