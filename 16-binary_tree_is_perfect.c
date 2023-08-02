ccbf19bae5cc3fff088ec0b2ad8743ca32bf1a6b
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-03 02:51:28 +0300

    minor changes

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,16 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left;
-	size_t height_right;
-
 	if (tree == NULL)
 		return (0);
 
-	height_left = binary_tree_height(tree->left);
-	height_left++;
-	height_right = binary_tree_height(tree->right);
+	size_t height_right = binary_tree_height(tree->right);
 	height_right++;
+	size_t height_left = binary_tree_height(tree->left);
+	height_left++;
 
 	if (height_left > height_right)
 		return (height_left);
