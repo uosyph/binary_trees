13e4339399454e9c06d1c52fbbde788693a3b301
Yousef Saeed <yousafesaeed@gmail.com>
2023-08-02 04:13:53 +0300

    Find uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -24,3 +24,21 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	return (NULL);
 }
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ *
+ * @node: Pointer to the node to find the uncle
+ *
+ * Return: Pointer to the uncle node,
+ * or NULL if node is NULL, or node has no uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL ||
+		node->parent == NULL ||
+		node->parent->parent == NULL)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
